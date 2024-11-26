e42c77b910f396ac95e9645f83630c8db8126c55
Anna <acesbrama@gmail.com>
2024-11-26 15:34:54 -0300

    feat: criaçãod a estrutura do component sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ILogo } from 'src/app/interface';
+import { ISidebarIcons } from 'src/app/interface/ISidebarIcons.interface';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
 
+export class SidebarComponent {
+  @Input() homeLink!: { route: string };
+
+  logoDesktop: ILogo[] = [
+    { name: "Logo Civitas", image: 'assets/civitas-logos/logo_civitas_sem_fundo.webp' }
+  ];
+
+  logoMobile: ILogo[] = [
+    { name: "Civitas - logo em azul com escrita em preto", image: 'assets/civitas-logos/logo_completo_horizontal_civitas.webp' }
+  ]
+
+  @Input() menuIcons: ISidebarIcons[] = [];
+
+  constructor(private router: Router) { }
+
+  //Roteamento dos icons do sidebar
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
+
+  logout(): void {
+    this.router.navigate(['/']);
+
+  }
 }
